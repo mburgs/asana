acd6f77fca76cf5fffebda1bf68f18bd085631c8
Michoel Burger <burgercho@gmail.com>
2015-01-03 11:29:05 -0500

    Update tests

---
+++ test/mocks.py
@@ -0,0 +1,20 @@
+class ApiMock():
+	"""Mock an API by saving all calls in a dict"""
+
+	def __init__(self):
+		self.requests = []
+
+	def get(self, target, **kwargs):
+		self._savecall('get', target, **kwargs)
+
+	def post(self, target, **kwargs):
+		self._savecall('post', target, **kwargs)
+
+	def put(self, target, **kwargs):
+		self._savecall('put', target, **kwargs)
+
+	def delete(self, target, **kwargs):
+		self._savecall('delete', target, **kwargs)
+
+	def _savecall(self, method, target, **kwargs):
+		self.requests.append((method, target, kwargs))
--- test/test.py
+++ test/test.py
@@ -1,48 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, unittest
+import sys, unittest, re
 
 sys.path.insert(0, "../")
 asana =  __import__('asana')
 
 from asana import *
 
-class ApiMock():
-	"""Mock an API by saving all calls in a dict"""
-	pass
+from mocks import ApiMock
 
-class GeneralTest(unittest.TestCase):
+class BaseTest(unittest.TestCase):
+	def setUp(self):
+		self.api = ApiMock()
+		Entity.set_api(self.api)
+
+	def slugify(self, name):
+	    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+	    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+class GeneralTest(BaseTest):
 	"""Tests that affect multiple classes"""
 	def test_section_definition(self):
-		section = {'name': 'Foo:'}
-		task = {'name': 'Foo'}
+		"""Ensure section definition is correct in
+		both Task and Section"""
+		tests = {
+			True:{'name': 'Foo:'},
+			False:{'name': 'Foo'}
+		}
 
-		self.assertFalse(Task._is_section(task))
-		self.assertFalse(Section._is_section(task))
+		for truth, ent in tests.items():
+			for cls in [Task, Section]:
+				self.assertEqual(cls._is_section(ent), truth)
 
-		self.assertTrue(Task._is_section(section))
-		self.assertTrue(Section._is_section(section))
-
-class EntityTest(unittest.TestCase):
-
-	def setUp(self):
-		pass
-
+class EntityTest(BaseTest):
 	def test_entity_data_getter(self):
 		"""Ensure data saved in is accessible"""
 		test_entity = Entity({'foo': 'bar'})
 
 		self.assertEqual(test_entity.foo, 'bar')
 
-class ProjectTest(unittest.TestCase):
+class ProjectTest(BaseTest):
 	def test_endpoint_correct(self):
 		self.assertEqual(Project._get_api_endpoint(), 'projects')
 
-class SectionTest(unittest.TestCase):
+class SectionTest(BaseTest):
 	def test_endpoint_correct(self):
 		"""Test section endpoint uses tasks"""
 		self.assertEqual(Section._get_api_endpoint(), 'tasks')
+
+class TaskTest(BaseTest):
+	def test_addremove_project(self):
+		"""Tests adding and removing project
+		with a plain id and with an object"""
+
+		task = Task({'id':1})
+
+		projects = {
+			2: Project({'id':2}),
+			3: 3
+		}
+
+		operations = ['addProject', 'removeProject']
+
+		for op in operations:
+			for id, obj in projects.items():
+				getattr(task, self.slugify(op))(obj)
+
+				self.assertIn(
+					('post', 'tasks/1/' + op, {'data': {'project': id}}),
+					self.api.requests
+				)
+
+
 
 if __name__ == "__main__":
 	unittest.main()
