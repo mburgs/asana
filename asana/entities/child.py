e7bf84155de2fa6ed103c38bd4e12d0523dae45f
Michoel Burger <burgercho@gmail.com>
2015-01-12 21:55:21 -0500

    don't need child file

--- asana/entities/child.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class Child(object):
-	def __init__(self, cls):
-		self.cls = cls
-
-	def get_value(self, parent):
-		return parent.get_subitem(self.cls)
