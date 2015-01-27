3acdddb7472a0d4c93a87fd7f2dcf7514518789e
Michoel Burger <burgercho@gmail.com>
2015-01-27 10:09:31 -0500

    Throw exception on attempt to load User from link

--- asana/entities/user.py
+++ asana/entities/user.py
@@ -4,3 +4,7 @@ class User(Entity):
 	_matchon = 'assignee$|followers|_by'
 
 	_fields = ['name', 'email', 'workspaces']
+
+	@classmethod
+	def from_link(cls, link):
+		raise Exception('This method does not work for the User type')
