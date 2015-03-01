2522abb9ce053ed41fbec5417badb91b5e4bbd71
Michoel Burger <burgercho@gmail.com>
2015-03-01 12:35:47 -0500

    Simplify API mock

--- test/mocks.py
+++ test/mocks.py
@@ -1,24 +1,18 @@
+from functools import partial
+
 class ApiMock():
 	"""Mock an API by saving all calls in a dict"""
 
 	def __init__(self):
 		self.requests = []
 
-	def get(self, target, **kwargs):
-		self._savecall('get', target, **kwargs)
+	def __nonzero__(self):
+		return True
 
-		return {}
-
-	def post(self, target, **kwargs):
-		self._savecall('post', target, **kwargs)
-
-		return {}
-
-	def put(self, target, **kwargs):
-		self._savecall('put', target, **kwargs)
-
-	def delete(self, target, **kwargs):
-		self._savecall('delete', target, **kwargs)
+	def __getattr__(self, attr):
+		return partial(self._savecall, attr)
 
 	def _savecall(self, method, target, **kwargs):
 		self.requests.append((method, target, kwargs))
+
+		return {}
